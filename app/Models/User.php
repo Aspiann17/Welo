caad8e67f6ffcff028eea0111496a89196ee7332
Aspian <p.aspian1738@gmail.com>
2024-12-18 17:43:13 +0800

    fix: resolve ProfileTest error when User model implements SoftDeletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     protected $fillable = [
         'name',
